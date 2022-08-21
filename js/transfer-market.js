d1bca631365e71437edb78c7f69af5fe500f194f
Atikul-Islam <atikmina25@gmail.com>
2022-08-22 03:55:23 +0600

    integreted selection list

---
+++ js/transfer-market.js
@@ -0,0 +1,100 @@
+document.getElementById('select1').addEventListener('click',function(){
+    const pl = document.getElementById("leo");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select1');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select2').addEventListener('click',function(){
+    const pl = document.getElementById("cr7");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select2');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select3').addEventListener('click',function(){
+    const pl = document.getElementById("neymar");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select3');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select4').addEventListener('click',function(){
+    const pl = document.getElementById("dembele");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select4');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select5').addEventListener('click',function(){
+    const pl = document.getElementById("mbappe");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select5');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select6').addEventListener('click',function(){
+    const pl = document.getElementById("lewa");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select6');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select7').addEventListener('click',function(){
+    const pl = document.getElementById("fati");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select7');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select8').addEventListener('click',function(){
+    const pl = document.getElementById("ramos");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select8');
+    select.setAttribute('disabled',true);
+})
+document.getElementById('select9').addEventListener('click',function(){
+    const pl = document.getElementById("paredes");
+    const pl1 = pl.innerText;
+
+    const list1 = document.getElementById('selected-list');
+    const li = document.createElement('li');
+    li.innerText = pl1;
+    list1.appendChild(li);
+    const select = document.getElementById('select9');
+    select.setAttribute('disabled',true);
+})
+
